7570a5847c38cb3095e6172bd7c7ffdb99c3d726
Henintsoa Paul <henintsoapaul@gmail.com>
2024-12-03 10:57:16 +0300

    [init]-First commit Seting up the project...

---
+++ src/main/java/mada_immo/MadaImmoApiApplication.java
@@ -0,0 +1,13 @@
+package mada_immo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadaImmoApiApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( MadaImmoApiApplication.class, args );
+    }
+
+}
---
+++ src/test/java/mada_immo/MadaImmoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mada_immo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadaImmoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
